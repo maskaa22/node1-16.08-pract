cedbdf49227784ea2d694658320c4fa73d1bbd0c
MASHa <MA77777SHa>
2021-08-18 23:32:26 +0300

    users

---
+++ practic/users.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {name: "Olya", gender: "female", age: 21},
+    //{name: "Valya", gender: "female", age: 22},
+    // {name: "Natasha", gender: "female", age: 23},
+     {name: "Lena", gender: "female", age: 14},
+    // {name: "Ulya", gender: "female", age: 15},
+    // {name: "Vasya", gender: "male", age: 21},
+    // {name: "Petya", gender: "male", age: 22},
+    // {name: "Sasha", gender: "male", age: 23},
+    // {name: "Seryu", gender: "male", age: 14},
+    // {name: "Vitalik", gender: "male", age: 15}
+]
