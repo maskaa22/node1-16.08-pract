e1f961ea98ec1945a9b9f9b52293e1965d44a111
MASHa <MA77777SHa>
2021-08-19 13:21:06 +0300

    users for file

--- app.js
+++ app.js
@@ -1,77 +1,20 @@
-//домашка
 const fs = require('fs');
 const path = require('path');
-const womenPath = path.join(__dirname, '1800');
-const manPath = path.join(__dirname, '2000');
-//
-// fs.readdir(womenPath, (err, files) => {
-//     if (err) {
-//         console.log(err);
-//         return
-//     }
-//     files.forEach(file => {
-//         const filePath = path.join(womenPath, file)
-//         const namePathFile = path.basename(filePath)
-//         const filenamePathNew = path.join(__dirname, '2000', namePathFile);
-//
-//         fs.readFile(filePath, (err1, data) => {
-//             if (err) {
-//                 console.log(err);
-//                 return
-//             }
-//             const arr = JSON.parse(data.toString());
-//             if (arr.gender === "male") {
-//                 fs.rename(filePath, filenamePathNew, err2 => {
-//                     console.log(err2);
-//                 })
-//             }
-//
-//         })
-//     })
-// })
-//
-// fs.readdir(manPath, (err, files) => {
-//     if (err) {
-//         console.log(err);
-//         return
-//     }
-//     files.forEach(file => {
-//         const filePath = path.join(manPath, file)
-//         const namePathFile = path.basename(filePath)
-//         const filenamePathNew = path.join(__dirname, '1800', namePathFile);
-//
-//         fs.readFile(filePath, (err1, data) => {
-//             if (err) {
-//                 console.log(err);
-//                 return
-//             }
-//             const arr = JSON.parse(data.toString());
-//             if (arr.gender === "female") {
-//                 fs.rename(filePath, filenamePathNew, err2 => {
-//                     console.log(err2);
-//                 })
-//             }
-//
-//         })
-//     })
-// })
-//
-
 
 const users = require('./practic/users');
+
 for (let user of users) {
 
     const userToString = JSON.stringify(user);
+    const userName = `${user.name}.txt`;
 
-    const usersDirectori = path.join(__dirname, user.name);
-    const manOlder20 = path.join(__dirname, 'manOlder20', user.name);
-    const manYounger20 = path.join(__dirname, 'manYounger20', user.name);
-    const womanOlder20 = path.join(__dirname, 'womanOlder20', user.name);
-    const womanYounger20 = path.join(__dirname, 'womanYounger20', user.name);
+    const usersDirectori = path.join(__dirname, userName);
+    const manOlder20 = path.join(__dirname, 'practic', 'manOlder20', userName);
+    const manYounger20 = path.join(__dirname, 'practic', 'manYounger20', userName);
+    const womanOlder20 = path.join(__dirname, 'practic', 'womanOlder20', userName);
+    const womanYounger20 = path.join(__dirname, 'practic', 'womanYounger20', userName);
 
-
-
-    fs.open(`${user.name}.txt`, 'w', err => {
+    fs.open(userName, 'w', err => {
         if (err) {
             console.log(err);
             return
@@ -79,21 +22,49 @@ for (let user of users) {
         console.log('File creted');
     });
 
-    fs.appendFile(`${user.name}.txt`, userToString, err => {
+    fs.appendFile(userName, userToString, err => {
         if (err) {
             console.log(err);
             return
         }
-        console.log('OK');
+        console.log('File - writing');
     });
 
-    if (user.age >= 20) {
-        //console.log(20);
-        if (user.gender === 'female') {
-            console.log(333);
-            fs.rename(usersDirectori, womanOlder20, err => {
-                console.log(err);
-            })
+    fs.readFile(usersDirectori, ((err, data) => {
+        if (err) {
+            console.log(err);
+            return
         }
-    } else console.log(111);
+
+        if (user.age >= 20) {
+            if (user.gender === 'male') {
+                fs.rename(usersDirectori, manOlder20, err => {
+                    console.log(err);
+                })
+                return;
+            }
+            if (user.gender === 'female') {
+                fs.rename(usersDirectori, womanOlder20, err => {
+                    console.log(err);
+                })
+            }
+            return;
+        }
+
+        if (user.age < 20) {
+            if (user.gender === 'male') {
+                fs.rename(usersDirectori, manYounger20, err => {
+                    console.log(err);
+                })
+                return;
+            }
+            if (user.gender === 'female') {
+                fs.rename(usersDirectori, womanYounger20, err => {
+                    console.log(err);
+                })
+            }
+            return;
+        }
+        console.log('Sorry');
+    }))
 }
